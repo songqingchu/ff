4e04cb22cd05feb603eed7a08c7f8fa031fff6b3
lijiayang <lijiayang@dianwoba.com>
2017-12-21 12:41:15 +0800

    other test2

--- src/main/java/com/a/cc/ff/App.java
+++ src/main/java/com/a/cc/ff/App.java
@@ -9,7 +9,10 @@ public class App
     public static void main( String[] args )
     {//fefe
     	//othoer:1
+    	//other:2
     	//test1
         System.out.println( "Hello World!" );
     }
+    
+    
 }
