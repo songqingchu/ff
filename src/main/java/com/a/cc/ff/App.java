13a8fe168d71ed2df53bb9c437cf8cc7a7a1e0bf
lijiayang <lijiayang@dianwoba.com>
2017-12-21 14:21:57 +0800

    再次清理

--- src/main/java/com/a/cc/ff/App.java
+++ src/main/java/com/a/cc/ff/App.java
@@ -6,16 +6,11 @@ package com.a.cc.ff;
  */
 public class App 
 {
-	int a2;
-	
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
     
-    
-    void method21() {
-    	
-    }
     
 }
